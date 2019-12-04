bb57b591cd6b18629a63421082ec3207bc81169b
AFletch95 <51311308+AFletch95@users.noreply.github.com>
2019-12-03 17:00:01 -0800

    gameboard and character cards

---
+++ src/components/CharacterCards/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CharacterCards(props) {
+  return(
+    <div className="card">
+      <img className="card-img-top card-height" alt="" />
+    </div>
+  )
+}
---
+++ src/components/Gameboard/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Gamboard( props ) {
+  return (
+    <div className="container d-flex flex-wrap justify-content-center">
+      <h1>Cards go here</h1>
+    </div>
+  )
+}
